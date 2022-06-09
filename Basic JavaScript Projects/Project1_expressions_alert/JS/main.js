182e3fcf725eaa279f9da82be48d8e38a80b4bbe
Alisak1 <contactalisa@me.com>
2022-06-09 02:53:46 -0700

    First JavaScript Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+alert("Hello! Welcome to my JavaScript Webpage.");//Window Alert text
+document.write("I am learning JavaScript. ");//First line on webpage
+var B='Expression';//Variable declaration for concatenate string on next line
+document.write('Here is an example of an ' + B + ' value:')//Concatenate string
+document.write(10*10);//Expression
+var Cat = " Sprinkle", Dog = " Mochi", Bird = " Captain Mike"; //Defining multiple variables.
+document.write(Dog)  //Calling one particular variable.
