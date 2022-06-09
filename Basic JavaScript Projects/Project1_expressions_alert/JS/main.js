cab834bda7701b39871bf0ab46a49dbcedafe3cd
Alisak1 <contactalisa@me.com>
2022-06-09 03:17:03 -0700

    updated
    added another string variable

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,9 @@
 alert("Hello! Welcome to my JavaScript Webpage.");//Window Alert text
 document.write("I am learning JavaScript. ");//First line on webpage
-var B='Expression';//Variable declaration for concatenate string on next line
-document.write('Here is an example of an ' + B + ' value:')//Concatenate string
+var A='Here is an example of an ';//Variable declaration #1
+var B='Expression';//Variable declaration #2
+document.write(A + B + ' value:')//Concatenate of strings
 document.write(10*10);//Expression
 var Cat = " Sprinkle", Dog = " Mochi", Bird = " Captain Mike"; //Defining multiple variables.
 document.write(Dog)  //Calling one particular variable.
+
