2039fa5fc50a87fa190aca0c180508a5d1c406b3
Alisak1 <contactalisa@me.com>
2022-06-11 00:17:45 -0700

    Project2_functions Assigment
    contains:
    function += operator
    HTML element
    commenting

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+
+function myFunction() { //function that uses the += operator
+    var sentence="Hello!"; //first concatenate string
+    sentence += " My name is Alisa."; //second concatenate string
+    document.getElementById("Operator").innerHTML=sentence;//Above will show when called by HTML element
+}
