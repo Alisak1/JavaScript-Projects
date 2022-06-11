2a314000704ffd4da2d6d42552c66c2ff52e6030
Alisak1 <contactalisa@me.com>
2022-06-11 12:39:35 -0700

    Project4_dictionaries Assignment
    Incomplete, need help.

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+function Dictionary() {
+    var Employee = {
+        FirstName:"Matt",
+        Birthdate:"06/11/88",
+        DateHired:"01/05/22",
+        Age:"34",
+        Citizenship:"US",
+    };
+    document.getElementById("Dictionary").innerHTML = Employee.Citizenship;
+}
+
+
