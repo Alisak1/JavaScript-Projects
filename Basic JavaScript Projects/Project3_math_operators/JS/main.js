f573130de8f041d0ca8c1b5fc37f8a1b2709f5ce
Alisak1 <contactalisa@me.com>
2022-06-11 11:07:43 -0700

    Project3_math_operators Assignment
    Displays the following:
    Add, Subtract, Multiply, and Divide functions
    Multiple Operators
    Modulus Operatore
    Remainder
    Increment
    Deincrement
    Random number

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,32 @@
+//Add Function
+function addition_myFunction() { 
+    var addition= 7+3;
+    document.getElementById("Math").innerHTML="7 + 3 = " + addition;
+}
+//Subtract Function
+function subtraction_myFunction() { 
+    var subtraction= 8-3;
+    document.getElementById("Math").innerHTML="8 - 3 = " + subtraction;
+}
+//Multiply function
+function multiplication() { 
+    var simple_Math= 8*3;
+    document.getElementById("Math").innerHTML="8 * 3 = " + simple_Math;
+}
+//Divide function
+function division() { 
+    var simple_Math= 9/3;
+    document.getElementById("Math").innerHTML="9 / 3 = " + simple_Math;
+}
+//mulitple operators
+function more_Math() { 
+    var simple_Math= (9/3) * 3 + (2-1);
+    document.getElementById("Math").innerHTML="(9/3) * 3 + (2-1) = " + simple_Math;
+}
+//Modulus operator
+function modulus_Operator() { 
+    var simple_Math= 9 % 4;
+    document.getElementById("Math").innerHTML="9 % 4= " + simple_Math;
+}
+
+
